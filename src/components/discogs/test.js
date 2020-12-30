c1fc6978c0237c26cd7149f4733e4b38810fa97e
Jake <ekajb97@gmail.com>
2020-12-30 17:34:38 +0000

    Table layout sample

--- src/components/discogs/test.js
+++ src/components/discogs/test.js
@@ -6,27 +6,26 @@ var consumerKey = 'gropdjhgfQQWBoVmCokp'
 var consumerSecret = 'grNqFcJJFkfarIuSAdbWECHemNeoSPOC'
 
 // Authenticate by user token
-var dis1 = new Discogs({ userToken: 'emQcmnEHCrfaCCcxwTrVFhvEdRckNgIfiImhaGQC' });
-
-
-var dis3 = new Discogs('MyUserAgent/1.0', { userToken: 'emQcmnEHCrfaCCcxwTrVFhvEdRckNgIfiImhaGQC' });
-
 var db = new Discogs({ userToken: 'emQcmnEHCrfaCCcxwTrVFhvEdRckNgIfiImhaGQC' }).database();
 
-
+var id;
 
 
 class Disco extends React.Component {
 
     getDisc() {
         var searchParams = {
-            release_title: 'nevermind',
-            artist: 'nirvana'
+            release_title: 'Vulture Culture',
+            artist: 'Fangclub',
+            type: 'master'
+
 
         };
         db.search(searchParams)
             .then(function (obj) {
                 console.log(obj)
+                console.log("Discogs master ID needed for Marketplace Search: " +obj.results[0].master_id)
+                id = obj.results[0].master_id;
             });
     }
     render() {
--- src/components/spotify/SpotifyMisc.js
+++ src/components/spotify/SpotifyMisc.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Spotify from 'spotify-web-api-js'
+import {render} from "@testing-library/react";
+var Discogs = require('disconnect').Client;
 
 // Using Client Credentials Flow to Authenticate
 
+var db = new Discogs({ userToken: 'emQcmnEHCrfaCCcxwTrVFhvEdRckNgIfiImhaGQC' }).database();
 
 
 var spotifyApi = new Spotify();
 
+var id;
+var name;
+var album;
+var format = "Vinyl";
 
 
 class SpotifyMisc extends React.Component {
+
 	constructor() {
 		super();
 		const params = this.getHashParams();
@@ -34,9 +42,38 @@ class SpotifyMisc extends React.Component {
 	getMusic() {
 		spotifyApi.getMyTopTracks()
 			.then((response) => {
+
+
 				console.log(response.items);
+				console.log("Artist: " +response.items[0].artists[0].name)
+				console.log("Album: "+response.items[0].album.name)
+				console.log("Track Name: "+response.items[0].name)
+				name=response.items[0].artists[0].name;
+				album=response.items[0].album.name;
+				return name;
+				return album;
 			})
+		return name;
+		return album;
 	}
+
+	getDisc(name, album) {
+		var searchParams = {
+			release_title: name,
+			artist: album,
+			type: 'master'
+
+
+		};
+		db.search(searchParams)
+			.then(function (obj) {
+				console.log(obj)
+				console.log("Discogs master ID needed for Marketplace Search: " +obj.results[0].master_id)
+				console.log(obj.results[0].title)
+				id = obj.results[0].master_id;
+			});
+	}
+
 
 	render() {
 		return (
@@ -45,7 +82,18 @@ class SpotifyMisc extends React.Component {
 					<button>Login with Spotify</button>
 				</a>
 				<button onClick={this.getMusic}>Get Music</button>
+				<button onClick={this.getDisc}>Discogs x Spotify test</button>
+
+
+				<div className={"button"}>
+
+					<a href={"https://www.discogs.com/sell/list?master_id=" + db.id + "&ev=mb&format=" + format} >Buy Here!</a>
+
+
+				</div>
 			</div>
+
+
 
 		);
 	}
